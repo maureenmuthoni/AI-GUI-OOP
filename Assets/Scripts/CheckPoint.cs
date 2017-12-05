c801b9c9b8ec28c60dcab63964c849c8e811c652
maureenmuthoni <muthonimut@gmail.com>
2017-12-05 18:02:32 +1100

    Character Cust, Gamescene changes

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//this script can be found in the Component section under the option Character Set Up 
+//CheckPoint
+[AddComponentMenu("Character Set Up/CheckPoint")]
+public class CheckPoint : MonoBehaviour
+{
+    #region Variables
+    [Header("Check Point Elements")]
+    //GameObject for our currentCheckPoint
+    public GameObject curCheckPoint;
+    [Header("Character Handler")]
+    //character handler script that holds the players health
+    public CharacterHandler charH;   
+    #endregion
+    #region Start
+    void Start()
+    {
+        //the character handler is the component attached to our player
+        charH = this.GetComponent<CharacterHandler>();
+
+        #region Check if we have Key
+        //if we have a save key called SpawnPoint
+        if (PlayerPrefs.HasKey("SpawnPoint"))
+        {
+            //then our checkpoint is equal to the game object that is named after our save file
+            curCheckPoint = GameObject.Find(PlayerPrefs.GetString("SpawnPoint"));
+            //our transform.position is equal to that of the checkpoint
+            this.transform.position = curCheckPoint.transform.position;
+        }
+        #endregion
+    }
+    #endregion
+
+        #region Update
+      void Update()
+
+            {
+            //if our characters health is less than or equal to 0
+            if (charH.curHealth == 0)
+            {
+                //our transform.position is equal to that of the checkpoint
+                this.transform.position = curCheckPoint.transform.position;
+                //our characters health is equal to full health
+                charH.curHealth = charH.maxHealth;
+                //character is alive
+                charH.alive = true;
+                //characters controller is active
+                CharacterController controller = this.GetComponent<CharacterController>();
+                controller.enabled = true;
+            }
+        }
+
+        #endregion
+
+        #region OnTriggerEnter
+        //Collider other
+        void OnTriggerEnter(Collider other)
+    {
+        //if our other objects tag when compared is CheckPoint
+        if (other.CompareTag("CheckPoint"))
+        {
+            //our checkpoint is equal to the other object
+            curCheckPoint = other.gameObject;
+            //save our SpawnPoint as the name of that object
+            PlayerPrefs.SetString("SpawnPoint", curCheckPoint.name);
+        }
+            }
+        }
+
+    #endregion
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+//this script can be found in the Component section under the option Character Set Up 
+//Interact
+[AddComponentMenu("Character Set Up/Interact")]
+public class PickUp : MonoBehaviour
+{
+    #region Variables
+    //We are setting up these variable and the tags in update for week 3,4 and 5
+    [Header("Player and Camera connection")]
+    //create two gameobject variables one called player and the other mainCam
+    public GameObject Player;
+    public GameObject mainCam;
+    #endregion
+    #region Start
+    void Start()
+    {
+
+        //connect our player to the player variable via tag
+        Player = GameObject.FindGameObjectWithTag("Player");
+        //connect our Camera to the mainCam variable via tag
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    #endregion
+    #region Update
+    void update()
+    {
+        //if our interact key is pressed
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //create a ray
+            Ray Interact;
+            //this ray is shooting out from the main cameras screen point center of screen
+            Interact = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+            //create hit info
+            RaycastHit hitinfo;
+            //if this physics raycast hits something within 10 units
+            if (Physics.Raycast(Interact, out hitinfo, 10))
+            {
+                #region NPC tag
+                //and that hits info is tagged NPC
+                if (hitinfo.collider.CompareTag("NPC"))
+                {
+                    //Debug that we hit a NPC
+                    Debug.Log("Hitthe NPC");
+                    //Dialogue dlg = hitinfo.transform.GetComponent<Dialogue>();
+                    //if (dlg != null)
+                    //{
+                    //    dlg.showDlg = true;
+
+                    //}
+                    #endregion
+                    #region Item
+                    //and that hits info is tagged Item
+                    if (hitinfo.collider.CompareTag("Item"))
+                    {
+
+                        //Debug that we hit an Item
+                        Debug.Log("Hit an Item");
+
+                    }
+                    #endregion
+
+                }
+
+            }
+        }
+
+
+        #endregion
+    }
+
+
+
+
+
+}
